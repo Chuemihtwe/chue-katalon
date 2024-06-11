31fbb2f8e9ea80b6cc64f9621f8d09eddcc4a35b
SIPDOMAIN\chuemimihtwee <chuemimihtwe5678@gmail.com>
2024-06-11 09:48:04 +0630

    2nd commit

---
+++ Test Suites/Appointment.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Appointment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a1ee5640-e3e5-4ed4-9ab8-c395fa559ce4</testSuiteGuid>
+   <testCaseLink>
+      <guid>0c7031ec-4a80-4b1a-a834-ff7772d08e4f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
